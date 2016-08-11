54f9988badf115927fca67be2de8e4e1a224907b
Vincenzo Olivito <vincenzo.olivito@beta80group.it>
2016-08-11 17:29:37 +0200

    new logic

--- facebook_bot.js
+++ facebook_bot.js
@@ -65,16 +65,25 @@ This bot demonstrates many of the core features of Botkit:
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+  config.get('validationToken');
 
-// if (!process.env.page_token) {
-//     console.log('Error: Specify page_token in environment');
-//     process.exit(1);
-// }
-// 
-// if (!process.env.verify_token) {
-//     console.log('Error: Specify verify_token in environment');
-//     process.exit(1);
-// }
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.get('pageAccessToken');
+
+if (!PAGE_ACCESS_TOKEN) {
+    console.log('Error: Specify page_token in environment or in configuration file');
+    process.exit(1);
+}
+
+if (!VALIDATION_TOKEN) {
+    console.log('Error: Specify verify_token in environment or in configuration file');
+    process.exit(1);
+}
 
 var Botkit = require('./lib/Botkit.js');
 var os = require('os');
@@ -98,8 +107,8 @@ if(ops.lt === false && ops.ltsubdomain !== null) {
 
 var controller = Botkit.facebookbot({
     debug: true,
-    access_token: (process.env.page_token) ? (process.env.page_token) : config.get('pageAccessToken'),
-    verify_token: (process.env.verify_token) ? (process.env.verify_token) : config.get('validationToken'),
+    access_token: PAGE_ACCESS_TOKEN,
+    verify_token: VALIDATION_TOKEN,
 });
 
 var bot = controller.spawn({
@@ -126,229 +135,74 @@ controller.setupWebserver(process.env.PORT || 5000, function(err, webserver) {
 });
 
 
-controller.hears(['hello', 'hi'], 'message_received', function(bot, message) {
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
+controller.hears(['start'],'message_received',function(bot,message) {
+  bot.startConversation(message, askObjectId);
+});
+
+askObjectId = function(response, convo) {
+  convo.ask("What's the id/nickname of object do you want to control?", function(response, convo) {
+    convo.say("OK");
+    askOperation(response, convo);
+    convo.next();
+  });
+}
+askOperation = function(response, convo) {
+  convo.ask("What action do you want to perform? Say TURN ON, TURN OFF, WINTER or SUMMER", [
+      {
+        pattern: 'TURN ON',
+        callback: function(response,convo) {
+          convo.say('OK!');
+          askTemperature(response, convo);
+          convo.next();
         }
-    });
-});
+      },
+      {
+        pattern: 'TURN OFF',
+        callback: function(response,convo) {
+          convo.say('Great! I will continue...');
+            askTemperature(response, convo);
+          convo.next();
 
-
-controller.hears(['structured'], 'message_received', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-        convo.ask({
-            attachment: {
-                'type': 'template',
-                'payload': {
-                    'template_type': 'generic',
-                    'elements': [
-                        {
-                            'title': 'Classic White T-Shirt',
-                            'image_url': 'http://petersapparel.parseapp.com/img/item100-thumb.png',
-                            'subtitle': 'Soft white cotton t-shirt is back in style',
-                            'buttons': [
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/view_item?item_id=100',
-                                    'title': 'View Item'
-                                },
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/buy_item?item_id=100',
-                                    'title': 'Buy Item'
-                                },
-                                {
-                                    'type': 'postback',
-                                    'title': 'Bookmark Item',
-                                    'payload': 'White T-Shirt'
-                                }
-                            ]
-                        },
-                        {
-                            'title': 'Classic Grey T-Shirt',
-                            'image_url': 'http://petersapparel.parseapp.com/img/item101-thumb.png',
-                            'subtitle': 'Soft gray cotton t-shirt is back in style',
-                            'buttons': [
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/view_item?item_id=101',
-                                    'title': 'View Item'
-                                },
-                                {
-                                    'type': 'web_url',
-                                    'url': 'https://petersapparel.parseapp.com/buy_item?item_id=101',
-                                    'title': 'Buy Item'
-                                },
-                                {
-                                    'type': 'postback',
-                                    'title': 'Bookmark Item',
-                                    'payload': 'Grey T-Shirt'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            }
-        }, function(response, convo) {
-            // whoa, I got the postback payload as a response to my convo.ask!
-            convo.next();
-        });
-    });
-});
-
-controller.on('facebook_postback', function(bot, message) {
-
-    bot.reply(message, 'Great Choice!!!! (' + message.payload + ')');
-
-});
-
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'message_received', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
         }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'message_received', function(bot, message) {
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
+      },
+      {
+        pattern: 'SUMMER',
+        callback: function(response,convo) {
+          convo.say('Great! I will continue...');
+            askTemperature(response, convo);
+          convo.next();
         }
-    });
-});
-
-controller.hears(['shutdown'], 'message_received', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
+      },
+      {
+        pattern: 'WINTER,
+        callback: function(response,convo) {
+          convo.say('Great! I will continue...');
+            askTemperature(response, convo);
+          convo.next();
         }
-        ]);
-    });
-});
-
-
-controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], 'message_received',
-    function(bot, message) {
-
-        var hostname = os.hostname();
-        var uptime = formatUptime(process.uptime());
-
-        bot.reply(message,
-            ':|] I am a bot. I have been running for ' + uptime + ' on ' + hostname + '.');
-    });
-
-
-
-controller.on('message_received', function(bot, message) {
-    bot.reply(message, 'Try: `what is my name` or `structured` or `call me captain`');
-    return false;
-});
-
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
+      }
+      {
+        default: true,
+        callback: function(response,convo) {
+          convo.say('Please insert one of the suggested answer');
+          convo.repeat();
+          convo.next();
+        }
+      }
+    ]);
+}
+askTemperature = function(response, convo) { 
+  convo.ask("What temperature do you want?", function(response, convo) {
+    convo.say("Ok! The last step.");
+    askFanVelocity(response, convo);
+    convo.next();
+  });
+}
+askFanVelocity = function(response, convo) { 
+  convo.ask("What temperature do you want?", function(response, convo) {
+    convo.say("Perfect! I update your object");
+    askFanVelocity(response, convo);
+    convo.next();
+    
+  });
 }
