1b775eba36a8eaa6f122238fd4d07fe39374bc24
Vincenzo Olivito <vincenzo.olivito@beta80group.it>
2016-09-11 20:06:10 +0200

    fix

--- facebook_bot.js
+++ facebook_bot.js
@@ -318,13 +318,11 @@ askObjectPassword = function(response, convo) {
                 }
                 else{
                 	convo.say("Password errata. Riprova o chiudi la chat.");
-                	convo.repeat();
-                	convo.next();
+                	convo.stop();
                 }
             } else {
                 convo.say("Password errata. Riprova o chiudi la chat.");
-                convo.repeat();
-                convo.next();
+                convo.stop();
             }
         });
 
