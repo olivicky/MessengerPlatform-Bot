b1ce17b26e772712993a6aca3fe1cf8fdc9f3a50
Vincenzo Olivito <vincenzo.olivito@gmail.com>
2017-09-24 19:34:25 +0200

    allineamento con versione di produzione

--- facebook_bot.js
+++ facebook_bot.js
@@ -4,65 +4,37 @@
           \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
            \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
             \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
 This is a sample Facebook bot built with Botkit.
-
 This bot demonstrates many of the core features of Botkit:
-
 * Connect to Facebook's Messenger APIs
 * Receive messages based on "spoken" patterns
 * Reply to messages
 * Use the conversation system to ask questions
 * Use the built in storage system to store and retrieve information
   for a user.
-
 # RUN THE BOT:
-
   Follow the instructions here to set up your Facebook app and page:
-
     -> https://developers.facebook.com/docs/messenger-platform/implementation
-
   Run your bot from the command line:
-
     page_token=<MY PAGE TOKEN> verify_token=<MY_VERIFY_TOKEN> node facebook_bot.js [--lt [--ltsubdomain LOCALTUNNEL_SUBDOMAIN]]
-
   Use the --lt option to make your bot available on the web through localtunnel.me.
-
 # USE THE BOT:
-
   Find your bot inside Facebook to send it a direct message.
-
   Say: "Hello"
-
   The bot will reply "Hello!"
-
   Say: "who are you?"
-
   The bot will tell you its name, where it running, and for how long.
-
   Say: "Call me <nickname>"
-
   Tell the bot your nickname. Now you are friends.
-
   Say: "who am I?"
-
   The bot will tell you your nickname, if it knows one for you.
-
   Say: "shutdown"
-
   The bot will ask if you are sure, and then shut itself down.
-
   Make sure to invite your bot into other channels using /invite @<my bot>!
-
 # EXTEND THE BOT:
-
   Botkit has many features for building cool and useful bots!
-
   Read all about it here:
-
     -> http://howdy.ai/botkit
-
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 // Arbitrary value used to validate a webhook
 // const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
@@ -89,11 +61,11 @@ var commandLineArgs = require('command-line-args');
 var localtunnel = require('localtunnel');
 var request = require('request');;
 
-var url = 'http://dmautomation-domoticadomain.rhcloud.com';
+var url = 'https://domiwiiapp.herokuapp.com';
 var idQuestion = "Ciao quale DomiWii vuoi controllare?";
 var passwordQuestion = "Inserisci la password: "
 var modeQuestion = "Basta che digiti il comando o la sua iniziale...oppure scrivi Ciao per uscire!";
-var fanQuestion = "Che velocità vuoi impostare?(default 1)";
+var fanQuestion = "Che velocità vuoi impostare?";
 var temperatureQuestion = "Che temperatura vuoi impostare?";
 var confortQuestion = "Che indice benessere preferisci?";
 
@@ -324,7 +296,7 @@ askConfortIndex = function(response, convo) {
 
 askTemperature = function(response, convo, defaultValue) {
 	if(defaultValue != null){
-		temperatureQuestion = temperatureQuestion + "(default " +defaultValue + "°)";
+		temperatureQuestion = temperatureQuestion;
 	}
 		
     convo.ask(temperatureQuestion, function(response, convo) {
@@ -385,7 +357,7 @@ askRecap = function(response, convo) {
     	recapQuestion = "Sei sicuro di voler spegnere il condizionatore controllato da DomiWii (" + alias + ") ?";
     }
     else if(mode == "VENTILATORE" || mode == "Ventilatore" || mode == "ventilatore" || mode == "V" || mode == "v"){
-    	recapQuestion = "Sei sicuro di voler attivare il condizionatore controllato da DomiWii (" + alias + ") in modalità Ventilatore, Temperatura: 21°C e velocità " + velocity + "?";
+    	recapQuestion = "Sei sicuro di voler attivare il condizionatore controllato da DomiWii (" + alias + ") in modalità Ventilatore con velocità " + velocity + "?";
     }
     
     else{
